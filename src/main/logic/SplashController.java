108ac9da95c05a9481888e5a934f02bef7d04bb6
lucaterzino <lucaterzino56@gmail.com>
2025-12-04 03:18:52 +0100

    fix warning

--- src/main/logic/SplashController.java
+++ src/main/logic/SplashController.java
@@ -7,7 +7,6 @@ import javafx.animation.Timeline;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.canvas.Canvas;
-import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Button;
 import javafx.scene.control.ProgressBar;
 import javafx.scene.input.KeyCode;
